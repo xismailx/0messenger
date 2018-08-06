53690cd4b30790404d4182e97bbf50ff37729b39
xismailx <42072070+xismailx@users.noreply.github.com>
2018-08-06 17:50:24 +0200

    Add files via upload

--- message.py
+++ message.py
@@ -1,13 +1,14 @@
 import fbchat
+from fbchat.models import *
 from getpass import getpass
 username = str(raw_input("Username: "))
 client = fbchat.Client(username, getpass())
 no_of_friends = int(raw_input("Number of friends: "))
 for i in xrange(no_of_friends):
-    name = str(raw_input("Name: "))
-    friends = client.getUsers(name)  # return a list of names
-    friend = friends[0]
-    msg = str(raw_input("Message: "))
-    sent = client.send(friend.uid, msg)
-    if sent:
-        print("Message sent successfully!")
+name = str(raw_input("Name: "))
+friends = client.searchForUsers(name) # return a list of names
+friend = friends[0]
+msg = str(raw_input("Message: "))
+sent = client.send(Message(text=msg), thread_id=friend.uid, thread_type=ThreadType.USER)
+if sent:
+print("Message sent successfully!")
